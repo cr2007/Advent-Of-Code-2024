c0b55eb2cc1b612c0d731da6f35a8ee085e3e909
CSK <73425927+cr2007@users.noreply.github.com>
2024-12-05 12:10:37 +0400

    Adds Day 1 Part 2 Java, Python solutions

---
+++ java/src/main/java/Day01_Historian_Hysteria/Day1_Part2.java
@@ -0,0 +1,55 @@
+package Day01_Historian_Hysteria;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+public class Day1_Part2 {
+    public static void main(String[] args) {
+        List<Integer> leftList = new ArrayList<>();
+        List<Integer> rightList = new ArrayList<>();
+        List<Integer> similarityScore = new ArrayList<>();
+
+        String filePath = Objects.requireNonNull(
+                Day1_Part2.class.getResource("/Day1_input.txt")
+        ).getPath();
+        filePath = URLDecoder.decode(filePath, StandardCharsets.UTF_8);
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split("\\s+");
+
+                if (parts.length == 2) {
+                    leftList.add(Integer.parseInt(parts[0].trim()));
+                    rightList.add(Integer.parseInt(parts[1].trim()));
+                } else System.out.println("Skipping invalid line: " + line);
+            }
+        } catch (IOException e) {
+            System.out.println("File not found");
+        }
+
+        Collections.sort(leftList);
+        Collections.sort(rightList);
+
+
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+
+        for (int value : rightList) frequencyMap.put(
+                value, frequencyMap.getOrDefault(value, 0) + 1
+        );
+
+        for (int value : leftList) {
+            int frequency = frequencyMap.getOrDefault(value, 0);
+            similarityScore.add(value * frequency);
+        }
+
+        int totalSimilarityScore = similarityScore.stream().mapToInt(Integer::intValue).sum();
+
+        System.out.println("Total Similarity Score: " + String.format("%,d", totalSimilarityScore));
+    }
+}
---
+++ python/01-Historian_Hysteria/Day1-Part2.py
@@ -0,0 +1,38 @@
+# Advent of Code 2024
+# Solution - Day 1 Part 2 - Historian Hysteria
+# Author: CSK
+
+import timeit
+
+def split_lists(lines: list[str]) -> tuple[list[int], list[int]]:
+    left_list, right_list = [], []
+
+    for line in lines:
+        # Split the line and convert into integers
+        values: list[int] = list(map(int, line.split()))
+
+        # Split the values according to odd and even indices
+        left_list.extend(values[::2])
+        right_list.extend(values[1::2])
+
+    # Return sorted lists
+    return sorted(left_list), sorted(right_list)
+
+
+def main():
+    values: list[str] = []
+
+    with open("input.txt", "r", encoding="utf-8") as file:
+        values = file.read().splitlines()
+
+    # Splits the values and returns the sorted lists
+    left_list, right_list = split_lists(values)
+
+    # Calculates similarity score for the 2 lists
+    similarity_score: list[int] = [value * right_list.count(value) for value in left_list]
+
+    # Prints the total similarity score
+    print(f"Total Similarity score: {sum(similarity_score):,}")
+
+if __name__ == "__main__":
+    main()
